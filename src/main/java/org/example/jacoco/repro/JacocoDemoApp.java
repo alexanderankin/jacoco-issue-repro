8f95b049c581cad051d8c53a9232a05ca352094a
Dave Ankin <daveankin@gmail.com>
2023-11-10 20:27:06 -0500

    app starts

---
+++ src/main/java/org/example/jacoco/repro/JacocoDemoApp.java
@@ -0,0 +1,11 @@
+package org.example.jacoco.repro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JacocoDemoApp.class, args);
+    }
+}
